5de006dc7e995c7d84ed07727a3ca410bc73c6ee
sandbar <suzannelee@Suzannes-MacBook-Pro.local>
2013-09-24 02:36:20 -0700

    artists can be named

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Genrelizer::Application.config.secret_key_base = 'bee26f4a7a8d3c6b2201487a2e1962a5bb7bc93326eb2dc024f289af34ad77834692f2f7127ab6c6ffd93ba64d37fc67f6475d0b510a686f08014419d63f2a31'
