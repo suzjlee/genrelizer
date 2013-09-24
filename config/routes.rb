35573987e5f50980e09f32ba2f743d055ebb2a09
sandbar <suzannelee@Suzannes-MacBook-Pro.local>
2013-09-24 13:29:56 -0700

    can create genres

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Genrelizer::Application.routes.draw do
+  resources :genres
+  
   resources :songs
 
   resources :artists
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130924093657) do
+ActiveRecord::Schema.define(version: 20130924202353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "artists", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "genres", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
