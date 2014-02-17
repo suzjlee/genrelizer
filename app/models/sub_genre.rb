88419a5f38d2158df66b77ec62766164df7c9d6d
sandbar <suzannelee@Suzannes-MacBook-Pro.local>
2014-02-17 18:33:46 -0500

    no duplicate sub-genres

--- app/models/sub_genre.rb
+++ app/models/sub_genre.rb
@@ -1,4 +1,5 @@
 class SubGenre < ActiveRecord::Base
   belongs_to :parent_genre, :class_name => 'Genre'
   belongs_to :child_genre, :class_name => 'Genre'
+  validates_uniqueness_of :child_genre_id, :scope => :parent_genre_id
 end
